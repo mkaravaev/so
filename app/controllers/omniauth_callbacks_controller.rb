9e5731704d3d5577d335b02810fedc3d1eb8ee4f
mkaravaev <mkaravaev@gmail.com>
2014-06-26 21:03:49 +0400

    add redirection to OniauthCallbackController

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -16,7 +16,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{@user.authorizations.first.provider.capitalize}") if is_navigational_format?
+      set_flash_message :notice, :success, kind: provider if is_navigational_format?
+    # else
+    #   session["devise." + provider + "_data"] = request.env["omniauth.auth"]
+    #   redirect_to new_user_registration_url
     end
   end
 
